394ec5ea512e914a03d5e7b8c596539ae34c0c12
Tim  Connell <timconnell@Tims-MacBook-Pro.local>
2019-09-26 12:25:43 +1000

    Add utm paramater to static form code for attributon tracking

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,17 @@ import '../css/styles/sass/header.css';
 import {FORM} from '../data/Content';
 
 class Header extends Component {
+    constructor(props) {
+      super(props);
+
+      var utm_code = this.getUrlParamater('utm_code');
+      var utm_hidden_form_field = '<input id="utm_source_hidden" type="hidden" value="" name="UTMSOURCE" class="" id="mce-UTMSOURCE"/>'
+
+      this.state = {
+        form: FORM.replace('</form>', utm_hidden_form_field + '</form>')
+      }
+    }
+
     render(){
         return (
             <header className="anu-header">
@@ -20,22 +31,22 @@ class Header extends Component {
                   Crawford School <br />of Public Policy</div>
                   <div className="tagline-logo"><ResponsiveImage imageName="anu-logo" class="anu-logo" /></div>
                 </div>
-                
+
               </div>
-              
+
               <div className="bottom">
                 {/* <ResponsiveImage imageName="arrow" class="header-Down-Arrow"/> */}
                 <div className="scott"><p><strong>Amanda Lee</strong> is currently completing a Master of Public Policy</p></div>
                 <div className="economic"><p><strong>Are you ready to influence Australia's response to critical policy issues such as population growth, public health, and human rights?</strong></p></div>
                 <div className="hero"><ResponsiveImage imageName="hero" class="hero-img"/></div>
-                
+
               </div>
 
               <div className="formInner">
               <div className="formContainer">
               <p id="anchorBack">Applications for 2020 now open</p>
               <div className="notice"><p>Please register your details below if you would like to receive a follow up email from our team, including a detailed course guide.</p></div>
-              <div dangerouslySetInnerHTML={{__html: FORM}} />
+              <div dangerouslySetInnerHTML={{__html: this.state.form}} />
               {/* <button type="button" className="download-button" id="anchorBack">Register Now</button> */}
              </div>
               </div>
@@ -43,10 +54,31 @@ class Header extends Component {
               <p>"I appreciate the wide range of people I get to engage with on a daily basis. The Crawford student body is very diverse which lets me discuss topics with people with a different perspective than my own"</p>
               <p className="substitle">Scott Thomson, Master of Environmental and Resource Economics</p>
               </div> */}
-                
+
               </div>
-          </header> 
+          </header>
         )
+    }
+
+    /* Component mounted */
+    componentDidMount() {
+      alert('Page loaded - component rendered')
+      console.log(this.getUrlParamater('utm_code'));
+    }
+
+    /* Get URL Paramaters */
+    getUrlParamater(sParam) {
+      //Get Parameters from URL
+      var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+      sURLVariables = sPageURL.split("&"),
+        sParameterName,
+        i;
+      for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split("=");
+        if (sParameterName[0] === sParam) {
+          return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+      }
     }
 }
 
